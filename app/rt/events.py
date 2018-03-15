067df86550ef3f455ed9feee9d5762923eb69dd6
Juan José Lozano Gómez <julogo11@gmail.com>
2018-03-15 01:39:09 +0100

    Socket.IO code moved down to events module

--- app/rt/events.py
+++ app/rt/events.py
@@ -1,1 +1,26 @@
+from threading import Lock
 
+from flask import current_app
+
+from .. import socketio
+
+thread = None
+thread_lock = Lock()
+
+
+def notifications_job(app):
+    count = 0
+    with app.app_context():
+        step = int(app.config.get('STEP', 1))
+        while True:
+            socketio.sleep(10)
+            count += 1
+            socketio.emit('my_response', {'count': count}, namespace='/rt/notifications/')
+
+
+@socketio.on('connect', namespace='/rt/notifications/')
+def start_notifications_thread():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(notifications_job, current_app._get_current_object())
--- run.py
+++ run.py
@@ -5,29 +5,5 @@ from app import create_app, socketio
 
 app = create_app()
 
-from threading import Lock
-
-thread = None
-thread_lock = Lock()
-
-
-def notifications_job():
-    count = 0
-    with app.app_context():
-        step = int(app.config.get('STEP', 1))
-        while True:
-            socketio.sleep(10)
-            count += step
-            print("Count: {}".format(count))
-            socketio.emit('my_response', {'count': count}, namespace='/rt/notifications/')
-
-
-@socketio.on('connect', namespace='/rt/notifications/')
-def start_notifications_thread():
-    global thread
-    with thread_lock:
-        if thread is None:
-            thread = socketio.start_background_task(target=notifications_job)
-
 if __name__ == '__main__':
     socketio.run(app, debug=True)
